023c5fd2870f5ed5724b37aca860beaa800fa558
Manfred Bergmann <mb@software-by-mabe.com>
2020-09-02 14:53:31 +0200

    is_alive now returns boolean.

--- lib/rules.ml
+++ lib/rules.ml
@@ -2,7 +2,7 @@ type alive_state = Alive | Dead
 
 let is_alive neighbours state =
   match neighbours with
-  | n when n < 2 -> Dead
-  | n when (n = 2 || n = 3) && state = Alive -> Alive
-  | 3 -> Alive
-  | _ -> Dead
+  | n when n < 2 -> false
+  | n when (n = 2 || n = 3) && state = Alive -> true
+  | 3 -> true
+  | _ -> false
--- test/rules_test.ml
+++ test/rules_test.ml
@@ -2,19 +2,19 @@ open OUnit2
 open Lib.Rules
 
 let living_less_than_2 _ =
-  assert_equal Dead (is_alive 0 Alive);
-  assert_equal Dead (is_alive 1 Alive)
+  assert_equal false (is_alive 0 Alive);
+  assert_equal false (is_alive 1 Alive)
 
 let living_2_or_3_survives _ =
-  assert_equal Alive (is_alive 2 Alive);
-  assert_equal Alive (is_alive 3 Alive)
+  assert_equal true (is_alive 2 Alive);
+  assert_equal true (is_alive 3 Alive)
 
 let living_more_than_3_dies _ =
-  assert_equal Dead (is_alive 4 Alive);
-  assert_equal Dead (is_alive 5 Alive)
+  assert_equal false (is_alive 4 Alive);
+  assert_equal false (is_alive 5 Alive)
 
 let dead_3_becomes_alive _ =
-  assert_equal Alive (is_alive 3 Dead)
+  assert_equal true (is_alive 3 Dead)
 
 let tests =
   "Rules tests" >:::
@@ -24,4 +24,3 @@ let tests =
       "living_more_than_3_dies" >:: living_more_than_3_dies;
       "dead_3_becomes_alive" >:: dead_3_becomes_alive;
     ]
-
