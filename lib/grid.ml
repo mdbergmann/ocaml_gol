dac4e0a102d6cd55c3718b845267c4ba41cb212a
Manfred Bergmann <mb@software-by-mabe.com>
2020-09-02 20:51:28 +0200

    some simplifications

--- lib/grid.ml
+++ lib/grid.ml
@@ -1,5 +1,5 @@
 
-let get_grid_value grid row col =
+let grid_value grid row col =
   if row < 0 then 0
   else if col < 0 then 0
   else if row >= List.length grid then 0
@@ -9,17 +9,15 @@ let get_grid_value grid row col =
 let alive_neighbours grid row col =
   match grid with
   | [] -> 0
-  | g -> List.fold_left
-           (fun acc x -> acc+x)
-           0
-           [get_grid_value g (row-1) (col-1);
-            get_grid_value g (row-1) col;
-            get_grid_value g (row-1) (col+1);
+  | g -> List.fold_left (+) 0
+           [grid_value g (row-1) (col-1);
+            grid_value g (row-1) col;
+            grid_value g (row-1) (col+1);
 
-            get_grid_value g row (col-1);
-            get_grid_value g row (col+1);
+            grid_value g row (col-1);
+            grid_value g row (col+1);
 
-            get_grid_value g (row+1) (col-1);
-            get_grid_value g (row+1) col;
-            get_grid_value g (row+1) (col+1)]
+            grid_value g (row+1) (col-1);
+            grid_value g (row+1) col;
+            grid_value g (row+1) (col+1)]
 
